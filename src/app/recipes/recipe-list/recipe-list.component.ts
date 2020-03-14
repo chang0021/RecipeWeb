cbe7bb532c61f99f5eba781977b361a25fbc41a9
unknown <chang0021@hotmail.com>
2020-03-13 22:56:03 -0700

    commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('NapaDumpling', 'This is a recipe of making dumplings. ', 'https://omnivorescookbook.com/wp-content/uploads/2015/02/1502_How-to-Make-Chinese-Dumplings-From-Scratch_002.jpg'),
+    new Recipe('Chinese doughnuts ', 'This is a recipe of makeing Chinese doughnuts', 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Youtiao.jpg/250px-Youtiao.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
