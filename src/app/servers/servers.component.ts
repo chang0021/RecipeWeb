18789d73d11d829555171eaebb541a2ade6c9530
unknown <chang0021@hotmail.com>
2020-03-15 23:31:32 -0700

    Learned property binding, event binding and two way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false; 
+  serverCreationStatus = "No server was created!"; 
+  serverName="testServer"; 
 
   constructor() { 
     setTimeout(() => {
@@ -17,4 +19,11 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
   }
 
+  OnCreateServer(){
+    this.serverCreationStatus = "Server was created!"; 
+  }
+
+  OnUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value; 
+  }
 }
