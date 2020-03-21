a5915409119599774c146a13790775765b7b1bf3
unknown <chang0021@hotmail.com>
2020-03-20 23:49:04 -0700

    removed bootstrap4 changed to bootstrap@3 because the drop down doesn't work.  Added a lot of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,23 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServersComponent } from './servers/servers.component'; 
-import { ServerComponent } from './server/server.component'; 
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-
+import { RecipesComponent } from './recipes/recipes.component'; 
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component'; 
+import { HeaderComponent } from './header/header.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component'; 
 @NgModule({
   declarations: [
     AppComponent,
-    ServersComponent, 
-    ServerComponent, 
-    WarningAlertComponent,
-    SuccessAlertComponent
+    RecipesComponent,
+    RecipeDetailComponent, 
+    RecipeListComponent,
+    HeaderComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
