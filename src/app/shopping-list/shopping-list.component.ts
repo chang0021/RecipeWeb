cc2dcc0a1569615619d3d1521767852c528203e5
unknown <chang0021@hotmail.com>
2020-03-22 00:25:41 -0700

    added ingredient model and some page details

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients:ingredient[] = [
+    new ingredient("apples", 5),
+    new ingredient("orange", 2)
+  ]; 
   constructor() { }
 
   ngOnInit() {
