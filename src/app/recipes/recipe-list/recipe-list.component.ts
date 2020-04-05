77dbe167ead97b07530c1f081dca62f762463f56
unknown <chang0021@hotmail.com>
2020-04-04 23:55:14 -0700

    event and property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>(); 
   recipes: Recipe[] = [
     new Recipe('NapaDumpling', 'This is a recipe of making dumplings. ', 'https://omnivorescookbook.com/wp-content/uploads/2015/02/1502_How-to-Make-Chinese-Dumplings-From-Scratch_002.jpg'),
     new Recipe('Chinese doughnuts ', 'This is a recipe of makeing Chinese doughnuts', 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Youtiao.jpg/250px-Youtiao.jpg')
@@ -14,6 +15,12 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
+  }
+
+  onRecipeSelected(recipeSelected: Recipe){
+    this.recipeWasSelected.emit(recipeSelected);
+    console.log("recipe list send out event! " + recipeSelected.name);  
   }
 
 }
