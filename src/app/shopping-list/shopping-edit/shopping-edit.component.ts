a9518a37d89aa0f40638f75128b2d221ce8a8820
unknown <chang0021@hotmail.com>
2020-04-07 00:23:32 -0700

    Use viewchild and output let shopedit "add" button works

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter} from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
+  @ViewChild("nameInput", {static:false}) nameInputRef: ElementRef; 
+  @ViewChild("amountInput", {static:false}) amountInputRef: ElementRef; 
+  @Output() itemAdded = new EventEmitter<Ingredient>(); 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  OnAddItem(){
+     const ingName = this.nameInputRef.nativeElement.value; 
+     const ingAmount = this.amountInputRef.nativeElement.value; 
+     const ing = new Ingredient(ingName, ingAmount); 
+     this.itemAdded.emit(ing); 
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ingredient } from '../shared/ingredient.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,13 +7,17 @@ import { ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients:ingredient[] = [
-    new ingredient("apples", 5),
-    new ingredient("orange", 2)
+  ingredients:Ingredient[] = [
+    new Ingredient("apples", 5),
+    new Ingredient("orange", 2)
   ]; 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAdded(newItem: Ingredient){
+    this.ingredients.push(newItem); 
   }
 
 }
